d8aeeaa918afb11799131bc735c6370069c1a9cd
aj3011 <ashreyjairath@gmail.com>
2023-05-15 17:15:07 -0700

    Fixing bad routes issue

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.use(express.static(__dirname + "/public"));
 //middleware to specify the use of transactionRouter for handling requests on the mentioned route
 app.use("/api/v1/transactions", transactionRouter);
 
+//my app on startup makes request to /, which is fine as once the request goes in, react router takes over and hijacks the routing. But on loadup, I landup at /login because of my client side logic.
+//Now if I refresh, the server tries to make a request to /login, which leads to an error saying "cannot GET /login". It is because the server has no routing defined for /login. To fix that, I do the following
+app.get("/*", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    };
+});
+
 app.listen(
   process.env.PORT || 3000,
   console.log(`Server running `.yellow.bold)
